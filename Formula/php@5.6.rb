2b98c8d7ba067300499450d33d2027eae99c97ce
Collin <collin@nb.cx>
2020-04-01 11:50:13 +0800

    del openssl

--- Formula/php@5.6.rb
+++ Formula/php@5.6.rb
@@ -148,7 +148,7 @@ class PhpAT56 < Formula
       --with-mysqli=mysqlnd
       --with-mysql=mysqlnd
       --with-ndbm#{headers_path}
-      --with-openssl=#{Formula["openssl"].opt_prefix}
+# --with-openssl=#{Formula["openssl"].opt_prefix}
       --with-pdo-dblib=#{Formula["freetds"].opt_prefix}
       --with-pdo-mysql=mysqlnd
       --with-pdo-odbc=unixODBC,#{Formula["unixodbc"].opt_prefix}
